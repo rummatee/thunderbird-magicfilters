0a7d4a29bb8cb549cc0cc28431d07b2dd27a6f69
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-07-07 22:07:53 +0200

    fixed a bug with simultanious messages

--- content/overlay.js
+++ content/overlay.js
@@ -16,26 +16,33 @@ var newMailListener = {
         var myPanel = document.getElementById("my-panel");
         if( !aMsgHdr.isRead ) {
 
-                    var recipients = aMsgHdr.recipients;
-                    var prettyName = aMsgHdr.folder.server.prettyName;
-                    var atPosition = prettyName.indexOf("@");
-                    var hostName = prettyName.substring(atPosition+1,prettyName.length);
-                    var userName = prettyName.substring(0,atPosition);
-                    var subject = aMsgHdr.subject;
-                    var listRecipients = recipients.split(" ");
-                    var parent = aMsgHdr.folder.rootFolder;
-                    magicFilter_listTargetDirs = [];
-                    var listLength = 0;
+                    myPanel.label = "create Object representing the target message";
+                    var target = {};
+                    myPanel.label = "created Object";
+                    target.recipients = aMsgHdr.recipients;
+                    myPanel.label = "set first value";
+                    target.prettyName = aMsgHdr.folder.server.prettyName;
+                    target.atPosition = target.prettyName.indexOf("@");
+                    target.hostName = target.prettyName.substring(target.atPosition+1,target.prettyName.length);
+                    target.userName = target.prettyName.substring(0,target.atPosition);
+                    myPanel.label = "set names";
+                    target.subject = aMsgHdr.subject;
+                    target.listRecipients = target.recipients.split(" ");
+                    target.parent = aMsgHdr.folder.rootFolder;
+                    target.listDestinations = [];
+                    target.listLength = 0;
+                    myPanel.label = "set all values";
                     if (prefs.getBoolPref("subaddressFilter")) {
-                        listRecipients.forEach(function(entry){
+                        target.listRecipients.forEach(function(entry){
                         
-                            if (entry.startsWith(userName+".") && entry.endsWith("@"+hostName)) {
+                            if (entry.startsWith(target.userName+".") && entry.endsWith("@"+target.hostName)) {
                                 
-                                var dirname = entry.substring(userName.length+1,entry.length-hostName.length-1);
+                                var dirname = entry.substring(target.userName.length+1,entry.length-target.hostName.length-1);
                                 
                                 myPanel.label = "call creating folder function";
-                                createSubfolderIfnotExists(parent,dirname);
-                                listLength++;
+                                createSubfolderIfnotExists(target,dirname);
+                                myPanel.label = "returned from function";
+                                target.listLength++;
                                 
                             }
                         });
@@ -45,28 +52,28 @@ var newMailListener = {
                     if (prefs.getBoolPref("mailinglistFilter")) {
                         var beginningIndex = 0;
                         var endIndex = 0;
-                        while (subject.includes("[",endIndex) && subject.includes("]",endIndex+1)) {
-                            beginningIndex = subject.indexOf("[",endIndex);
-                            endIndex = subject.indexOf("]",beginningIndex);
-                            mailinglistName = subject.substring(beginningIndex+1,endIndex);
+                        while (target.subject.includes("[",endIndex) && target.subject.includes("]",endIndex+1)) {
+                            beginningIndex = target.subject.indexOf("[",endIndex);
+                            endIndex = target.subject.indexOf("]",beginningIndex);
+                            mailinglistName = target.subject.substring(beginningIndex+1,endIndex);
                             myPanel.label = "found Mailinglist in subject. "+mailinglistName;
-                            createSubfolderIfnotExists(parent,mailinglistName);
-                            listLength++;
+                            createSubfolderIfnotExists(target,mailinglistName);
+                            target.listLength++;
                         }
                     }
-                    myPanel.label = "list Length: "+listLength;
+                    myPanel.label = "list Length: "+target.listLength;
                     setTimeout(function(){
                         myPanel.label = "now actually move message "+ magicFilter_listTargetDirs.length;
-                        if (magicFilter_listTargetDirs.length == 1) {
+                        if (target.listDestinations.length == 1) {
                             myPanel.label = "unambigues folder";
                             var cs = Components.classes["@mozilla.org/messenger/messagecopyservice;1"].getService(Components.interfaces.nsIMsgCopyService);
                             var mutableArray = Components.classes["@mozilla.org/array;1"].createInstance(Components.interfaces.nsIMutableArray);
                                 
                             mutableArray.appendElement(aMsgHdr, false /*weak*/);
-                            cs.CopyMessages(aMsgHdr.folder,mutableArray,magicFilter_listTargetDirs[0],1,null,msgWindow,true);
+                            cs.CopyMessages(aMsgHdr.folder,mutableArray,target.listDestinations[0],1,null,msgWindow,true);
                             myPanel.label = "moved message"
                         }
-                    }, 2000*listLength+2000);
+                    }, 2000*target.listLength+2000);
             }
 
                     
@@ -74,23 +81,23 @@ var newMailListener = {
 }
 
 
-function createSubfolderIfnotExists(parent,name) {
+function createSubfolderIfnotExists(target,name) {
         document.getElementById("my-panel").label = "function called";
         try {
-            parent.getChildNamed(name);
+            target.parent.getChildNamed(name);
         } catch (err) {
             document.getElementById("my-panel").label = "to create folder";
-            parent.createSubfolder(name,null);
+            target.parent.createSubfolder(name,null);
             
         }
         document.getElementById("my-panel").label = "to return folder";
         
         setTimeout(function(){
             document.getElementById("my-panel").label = "find folder to add";
-            var dir = parent.getChildNamed(name);
+            var dir = target.parent.getChildNamed(name);
             document.getElementById("my-panel").label = "found folder to add";
-            magicFilter_listTargetDirs.push(dir);
-            document.getElementById("my-panel").label = "added folder to list "+magicFilter_listTargetDirs.length;
+            target.listDestinations.push(dir);
+            document.getElementById("my-panel").label = "added folder to list "+target.listDestinations.length;
         }, 2000);
 }
 
