c1b64bbf77319915872fd0ac0cb25ab7637c0231
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-07-11 20:08:12 +0200

    remove debugging

--- content/filter.js
+++ content/filter.js
@@ -6,7 +6,6 @@ var prefs = Components.classes["@mozilla.org/preferences-service;1"]
 //the main Listener to filter Mails
 var newMailListener = {
     msgAdded: function(aMsgHdr) {
-        var myPanel = document.getElementById("my-panel");
         //if messages are read, we assume the user already handled them
         if( !aMsgHdr.isRead ) {
 
@@ -72,50 +71,37 @@ function createSubfolderIfnotExists(target,name) {
         //load preferences
         var parserMode = prefs.getComplexValue("parseMode", Components.interfaces.nsISupportsString).data;
         var substitute = prefs.getComplexValue("substitute", Components.interfaces.nsISupportsString).data;
-        document.getElementById("my-panel").label = "get Reverse Prefs";
         var parserReverse = prefs.getBoolPref("parseReverse");
         
         //if it is set, reverse the order of the parts of the name (seperated by dots)
         if (parserReverse) {
-            document.getElementById("my-panel").label = "reverse Parts";
             var partsList = name.split(".");
-            document.getElementById("my-panel").label = "split list";
             partsList.reverse();
-            document.getElementById("my-panel").label = "reverse List";
             name = partsList.join(".");
-            document.getElementById("my-panel").label = "Join List";
         }
 
         //if it is set replace dots
         if (parserMode=="replace") {
             name = name.replace(/\./g,substitute);
-            document.getElementById("my-panel").label = "replaced dots";
         }
 
         //check if folder exist and create if not
         try {
             target.parent.getChildNamed(name);
         } catch (err) {
-            document.getElementById("my-panel").label = "to create folder";
             target.parent.createSubfolder(name,null);//if there are dots in the name it automaticly creates subfolders
             
         }
         
         //give the server time to create the folder
         setTimeout(function(){
-            document.getElementById("my-panel").label = "find folder to add";
             var parent = target.parent;
             //if it is set search through the tree of subdirs to find the right one
             if (parserMode=="subfolders") {
-                document.getElementById("my-panel").label = "find subfolders";
                 while (name.includes(".")) {
-                    document.getElementById("my-panel").label = name.indexOf(".");
                     var subdir = name.substring(0, name.indexOf("."));
-                    document.getElementById("my-panel").label = subdir;
                     parent = parent.getChildNamed(subdir);
-                    document.getElementById("my-panel").label = "oldname: "+name;
                     name = name.substring(name.indexOf(".")+1,name.length);
-                    document.getElementById("my-panel").label = "name: "+name;
                 }
             }
 
